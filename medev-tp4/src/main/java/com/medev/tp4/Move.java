feaa4d2b0c8de8acf1103ff49b38c602e7d9a9f3
Yassss785 <yasminebouhadji02@gmail.com>
2025-12-08 16:23:12 +0100

    Creation of class Player , Move and Piece

---
+++ medev-tp4/src/main/java/com/medev/tp4/Move.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.medev.tp4;
+import java.io.Serializable;
+
+
+/**
+ *
+ * @author yasminebouhadji
+ */
+public class Move implements Serializable {
+    private final int fromRow, fromCol, toRow, toCol;
+
+    public Move(int fromRow, int fromCol, int toRow, int toCol) {
+        this.fromRow = fromRow;
+        this.fromCol = fromCol;
+        this.toRow = toRow;
+        this.toCol = toCol;
+    }
+
+    public int getFromRow() {
+        return fromRow;
+    }
+
+    public int getFromCol() {
+        return fromCol;
+    }
+
+    public int getToRow() {
+        return toRow;
+    }
+
+    public int getToCol() {
+        return toCol;
+    }
+}
