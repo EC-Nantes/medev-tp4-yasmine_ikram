486c0fec33400eab48fbae34d4aa0cde7dc9984a
ilyasbounoua <bounoua.ilyas@outlook.com>
2025-12-08 14:58:56 +0100

    Tests 101

---
+++ medev-tp4/src/main/java/com/medev/tp4/MedevTp4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.medev.tp4;
+
+/**
+ *
+ * @author Ilyas
+ */
+public class MedevTp4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
